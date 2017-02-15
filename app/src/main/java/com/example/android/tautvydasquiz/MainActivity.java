d7f4bd93aadfd2afdf5ffc20367b886fc74dfda3
Tautcius <t.bujauskas@gmail.com>
2017-02-15 22:38:30 +0200

    Added missing fields. questions with EditText works. not working with checkboxes - need to check conditional sentence not working radiobuttons.

--- app/src/main/java/com/example/android/tautvydasquiz/MainActivity.java
+++ app/src/main/java/com/example/android/tautvydasquiz/MainActivity.java
@@ -3,60 +3,106 @@ package com.example.android.tautvydasquiz;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
     int result = 0; //counting results;
-    /*
-    * Getting track of correct and wrong anwsers.
-     */
-    boolean resultForFirstQuesion, resultForSecondQuesion, resultForThirdQuesion, resultForForthQuesion,
-            resultForFithQuesion, resultForSixthQuesion, resultForSeventhQuesion, resultForEigthQuesion = false;
 
-//calculating result for first question;
-private String firstAnwserCalculation (String firstAnwserEntered){
-    if (firstAnwserEntered == "1918/02/16") {
-        result += 1;
-        resultForFirstQuesion = true;
-    }
-    return firstAnwserEntered;
-}
-//calculating result for second question;
-private String secondAnswerCalculation (String secondAnwserEntered){
-    if (secondAnwserEntered == "Mindaugas") {
-        resultForSecondQuesion = true;
-        result += 1;
-    }
-    return secondAnwserEntered;
-}
-public void getResults(View view){
+    private EditText firstAnswer, secondAnswer, fithAnwser, seventhAnwser;
+    private RadioGroup thirdQuestion, forthQuestion;
+    private RadioButton thirdAnwserRadio, forthAnserRadio;
+    private int thirdAnwser, forthAnwser;
+    boolean basketball, rowing, weightLifting, canoeing, boxing;
+    String resultForFirstQuesion, resultForSecondQuesion, resultForThirdQuesion, resultForForthQuesion, resultForFithQuesion, resultForSixthQuesion, resultForSeventhQuesion;
+
+    public void getResults(View view) {
     /*
     *geting anwser for first question;
-     */
-    EditText firstAnwser = (EditText) findViewById(R.id.firstAnwser);
-    String firstAnwserEntered = firstAnwser.getText().toString();
-    firstAnwserEntered= firstAnwserCalculation(firstAnwserEntered);
-
+    */
+        if (firstAnswer.getText().toString().equals("1918/02/16")) {
+            resultForFirstQuesion = "Correct";
+            result++;
+        } else {
+            resultForFirstQuesion = "Wrong";
+        }
     /*
-    *result for second question
-     */
-    EditText secondAnwser = (EditText) findViewById(R.id.secondAnwser);
-    String secondAnwserEntered = secondAnwser.getText().toString();
-    secondAnwserEntered = secondAnswerCalculation(secondAnwserEntered);
+    *geting anwser for second question;
+    */
+        if (secondAnswer.getText().toString().equals("Mindaugas")) {
+            resultForSecondQuesion = "correct";
+            result++;
+        } else {
+            resultForSecondQuesion = "wrong";
+        }
+    /*
+    *geting anwser for third question;
+    */
+        if (thirdAnwserRadio.getText().toString().equals("Less than 3 millions")) {
+            resultForFithQuesion = "correct";
+            result++;
+        } else {
+            resultForFithQuesion = "wrong";
+        }
+    /*
+    *geting anwser for forth question;
+    */
+        if (forthAnserRadio.getText().toString().equals("Less than 3 millions")) {
+            resultForFithQuesion = "correct";
+            result++;
+        } else {
+            resultForFithQuesion = "wrong";
+        }
+    /*
+    *geting anwser for fith question;
+    */
+        if (fithAnwser.getText().toString().equals("Basketball")) {
+            resultForFithQuesion = "correct";
+            result++;
+        } else {
+            resultForFithQuesion = "wrong";
+        }
+    /*
+    *geting anwser for sixth question;
+    */
+        if ((basketball && boxing) != (canoeing && rowing && weightLifting)) {
+            resultForSixthQuesion = "correct";
+            result++;
+        } else {
+            resultForSixthQuesion = "wrong";
+        }
+    /*
+    *geting anwser for seventh question;
+    */
+        if (seventhAnwser.getText().toString().equals("Vilnius")) {
+            resultForSeventhQuesion = "correct";
+            result++;
+        } else {
+            resultForSeventhQuesion = "wrong";
+        }
 
-    String resultMessage = createResultsReport(result, resultForFirstQuesion, resultForSecondQuesion);
 
-    displayResult(resultMessage);
-}
+        String resultMessage = createResultsReport(result, resultForFirstQuesion, resultForSecondQuesion, resultForFithQuesion, resultForSixthQuesion, resultForSeventhQuesion);
+        displayResult(resultMessage);
+        result = 0;
+    }
 
     /*
     *create results report
      */
-    public String createResultsReport(int result, boolean resultForFirstQuesion, boolean resultForSecondQuesion){
+    public String createResultsReport(int result, String resultForFirstQuesion, String resultForSecondQuesion, String resultForFithQuesion,
+                                      String resultForSixthQuesion, String resultForSeventhQuesion){
         String resultMessage = "You have anwsered to " + result + " questions corectly.";
         resultMessage += "\nAnwser for first question is " + resultForFirstQuesion;
         resultMessage += "\nAnwser for second question is " + resultForSecondQuesion;
+//        resultMessage += "\nAnwser for third question is " + resultForThirdQuesion;
+//        resultMessage += "\nAnwser for forth question is " + resultForForthQuesion;
+        resultMessage += "\nAnwser for fith question is " + resultForFithQuesion;
+        resultMessage += "\nAnwser for sixth question is " + resultForSixthQuesion;
+        resultMessage += "\nAnwser for seventh question is " + resultForSeventhQuesion;
         return resultMessage;
     }
 
@@ -64,7 +110,32 @@ public void getResults(View view){
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-           }
+
+        firstAnswer = (EditText)findViewById(R.id.firstAnwser);
+        secondAnswer = (EditText)findViewById(R.id.secondAnwser);
+        fithAnwser = (EditText)findViewById(R.id.fithAnwser);
+        CheckBox checkBoxBasketball, checkBoxRowing, checkBoxWheightLifting, checkBoxCanoeing, checkBoxBoxing;
+        checkBoxBasketball = (CheckBox)findViewById(R.id.checkbox_basketball);
+        basketball = checkBoxBasketball.isChecked();
+        checkBoxBoxing = (CheckBox)findViewById(R.id.checkbox_boxing);
+        boxing = checkBoxBoxing.isChecked();
+        checkBoxRowing = (CheckBox)findViewById(R.id.checkbox_rowing);
+        rowing = checkBoxRowing.isChecked();
+        checkBoxWheightLifting = (CheckBox)findViewById(R.id.checkbox_weightlifting);
+        weightLifting = checkBoxWheightLifting.isChecked();
+        checkBoxCanoeing = (CheckBox)findViewById(R.id.checkbox_canoeing);
+        canoeing = checkBoxCanoeing.isChecked();
+        seventhAnwser = (EditText)findViewById(R.id.seventhAnwser);
+
+        thirdQuestion = (RadioGroup)findViewById(R.id.third_question);
+        thirdAnwser = thirdQuestion.getCheckedRadioButtonId();
+        thirdAnwserRadio = (RadioButton)findViewById(thirdAnwser);
+        forthQuestion = (RadioGroup)findViewById(R.id.forth_question);
+        forthAnwser = forthQuestion.getCheckedRadioButtonId();
+        forthAnserRadio = (RadioButton)findViewById(forthAnwser);
+    }
+
+
     /*
     *Shows anwser
      */
@@ -73,3 +144,6 @@ public void getResults(View view){
         quizResultTextView.setText(message);
     }
 }
+
+
+
